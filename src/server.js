b655c3f249b659bb06887a6813f266c38c79cdae
Keith Johnson <kjohnson0451@gmail.com>
2023-09-20 11:13:01 -0400

    Switched from Koa.js to Express.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
-const Koa = require("koa")
-const app = new Koa()
+const express = require("express")
+const app = express()
+const port = 3000
 
-app.use(async (ctx) => {
-  ctx.body = "Hello World"
+app.get("/", (req, res) => {
+  res.send("Hello World!")
 })
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
