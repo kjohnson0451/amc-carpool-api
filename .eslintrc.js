c3e264f353d10897e410a9cf352b18698dc2d0e0
Keith Johnson <kjohnson0451@gmail.com>
2023-09-28 11:03:25 -0400

    Eslint rule - don't allow vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "never"],
+    // disable vars-on-top, and then disallow vars completely
+    "vars-on-top": "off",
+    "no-var": "error",
   },
   parserOptions: {
     ecmaVersion: 15,
