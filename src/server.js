2ecaa074f52e2d920c42ff8c22e73511c125f6e0
Keith Johnson <kjohnson0451@gmail.com>
2023-10-09 12:59:53 -0400

    Get rid of extensions on imports

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express"
-import logger from "@utils/logger.js"
-import requestLogger from "@middleware/logger/request-logger.js"
+import logger from "@utils/logger"
+import requestLogger from "@middleware/logger/request-logger"
 
 const app = express()
 const port = 3000
