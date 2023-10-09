f5faf1fe0660ce5ce3b69af1df501b8a12ad4979
Keith Johnson <kjohnson0451@gmail.com>
2023-10-09 12:55:47 -0400

    Forget about that 'logger' subdir

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express"
-import logger from "@utils/logger/logger.js"
+import logger from "@utils/logger.js"
 import requestLogger from "@middleware/logger/request-logger.js"
 
 const app = express()
