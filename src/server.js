b3f6109ab43cd27f78da72146d5b76de2267b97f
Keith Johnson <kjohnson0451@gmail.com>
2023-10-12 12:34:44 -0400

    Rename app variable to server, just because

--- src/server.js
+++ src/server.js
@@ -3,15 +3,11 @@ import "dotenv/config"
 import logger from "@utils/logger"
 import requestLogger from "@middleware/logger/request-logger"
 
-const app = express()
+const server = express()
 const port = 3000
 
-app.use(requestLogger)
+server.use(requestLogger)
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
-
-app.listen(port, () => {
+server.listen(port, () => {
   logger.info(`Server is running on port ${port}`)
 })
