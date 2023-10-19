51edf7319da8818e54953da5b97d16686050123f
Keith Johnson <kjohnson0451@gmail.com>
2023-10-19 16:37:00 -0400

    Installed and applied the helmet package

--- src/utils/load_middlewares.js
+++ src/utils/load_middlewares.js
@@ -1,9 +1,17 @@
+import helmet from "helmet"
 import requestLogger from "@middleware/logger/request_logger"
+import { NodeEnv } from "@config/environment_variables"
 
 // I might want to change this into a loop that iterates
 // through the src/middleware/ subdir, but this will do for now
 const loadMiddlewares = (server) => {
+  // Log every request made to the server
   server.use(requestLogger)
+
+  if (NodeEnv !== "development") {
+    // Use the helmet package to send security related HTTP headers
+    server.use(helmet())
+  }
 }
 
 export default loadMiddlewares
