ce9b098d0edacc957b5a89f41fdcac6db2ebcab5
Keith Johnson <kjohnson0451@gmail.com>
2023-10-23 17:07:28 -0400

    ParseInt the server port

--- src/config/environment_variables.js
+++ src/config/environment_variables.js
@@ -1,3 +1,3 @@
 export const PgUrl = process.env.PG_URL
 export const NodeEnv = process.env.NODE_ENV || "production"
-export const ServerPort = process.env.SERVER_PORT || 3000
+export const ServerPort = parseInt(process.env.SERVER_PORT) || 3000
