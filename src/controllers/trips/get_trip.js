e3ebe10de1b5efd575908fd828d953daf7da8acc
Keith Johnson <kjohnson0451@gmail.com>
2023-10-25 13:06:43 -0400

    Have get-trip route include participants and locations

--- src/controllers/trips/get_trip.js
+++ src/controllers/trips/get_trip.js
@@ -1,9 +1,21 @@
 import Trip from "@models/trip"
+import TripParticipant from "@models/trip_participant"
 import Participant from "@models/participant"
+import AddressLocation from "@models/address_location"
+import CoordinatesLocation from "@models/coordinates_location"
 
 const getTrip = async (req, res) => {
   const { tripId } = req.params
-  const trip = await Trip.findByPk(tripId, { include: Participant })
+  const trip = await Trip.findByPk(tripId, {
+    include: [
+      {
+        model: TripParticipant,
+        include: [Participant, AddressLocation, CoordinatesLocation],
+      },
+      AddressLocation,
+      CoordinatesLocation,
+    ],
+  })
   res.json(trip)
 }
 
