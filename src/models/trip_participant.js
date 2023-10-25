d4cd35c750093ad41c64f11d1fba93d61a3bc1be
Keith Johnson <kjohnson0451@gmail.com>
2023-10-25 10:52:33 -0400

    Get rid of association here

--- src/models/trip_participant.js
+++ src/models/trip_participant.js
@@ -1,7 +1,5 @@
 import db from "@utils/db"
 import { CarpoolSchema } from "@config/db_schemas"
-import AddressLocation from "@models/address_location"
-import CoordinatesLocation from "@models/coordinates_location"
 import hasJustOneOfAddressOrCoordinates from "@utils/validators/has_just_one_of_address_or_coordinates"
 
 const modelName = "TripParticipant"
@@ -18,11 +16,5 @@ const TripParticipant = db.define(
     },
   },
 )
-
-AddressLocation.hasMany(TripParticipant)
-TripParticipant.belongsTo(AddressLocation)
-
-CoordinatesLocation.hasMany(TripParticipant)
-TripParticipant.belongsTo(CoordinatesLocation)
 
 export default TripParticipant
