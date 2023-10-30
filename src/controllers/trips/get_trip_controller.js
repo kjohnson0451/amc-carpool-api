ca4908c2527fa8fb52036872ee82b6c9e175232f
Keith Johnson <kjohnson0451@gmail.com>
2023-10-30 17:39:19 -0400

    Try catch blocks and status codes

--- src/controllers/trips/get_trip_controller.js
+++ src/controllers/trips/get_trip_controller.js
@@ -1,16 +1,29 @@
 import getTripById from "@services/trips/get_trip_by_id"
-import { getTripSuccessMessage, statusSuccess } from "@config/strings"
+import {
+  getTripSuccessMessage,
+  statusSuccess,
+  statusError,
+} from "@config/strings"
 
 const getTrip = async (req, res) => {
-  const { id } = req.params
-  const status = statusSuccess
-  const code = 200
-  const message = getTripSuccessMessage
+  try {
+    const { id } = req.params
+    const status = statusSuccess
+    const code = 200
+    const message = getTripSuccessMessage
 
-  const trip = await getTripById(id)
+    const trip = await getTripById(id)
 
-  const returnedData = { status, code, message, trip }
-  res.status(code).json(returnedData)
+    const data = { status, code, message, trip }
+    res.status(code).json(data)
+  } catch (error) {
+    const status = statusError
+    const code = 500
+    const { message } = error
+
+    const data = { status, code, message }
+    res.status(code).json(data)
+  }
 }
 
 export default getTrip
--- src/controllers/trips/post_trip_controller.js
+++ src/controllers/trips/post_trip_controller.js
@@ -1,20 +1,33 @@
 import createTrip from "@services/trips/create_trip"
 import getTripById from "@services/trips/get_trip_by_id"
-import { createTripSuccessMessage, statusSuccess } from "@config/strings"
+import {
+  createTripSuccessMessage,
+  statusSuccess,
+  statusError,
+} from "@config/strings"
 
 const postTripController = async (req, res) => {
-  const tripData = req.body
-  const trip = await createTrip(tripData)
+  try {
+    const tripData = req.body
+    const trip = await createTrip(tripData)
 
-  const status = statusSuccess
-  const code = 201
-  const message = createTripSuccessMessage
-  const { id } = trip
+    const status = statusSuccess
+    const code = 201
+    const message = createTripSuccessMessage
+    const { id } = trip
 
-  const returnedTripData = await getTripById(id)
+    const returnedTripData = await getTripById(id)
 
-  const returnedData = { status, code, message, trip: returnedTripData }
-  res.status(code).json(returnedData)
+    const data = { status, code, message, trip: returnedTripData }
+    res.status(code).json(data)
+  } catch (error) {
+    const status = statusError
+    const code = 500
+    const { message } = error
+
+    const data = { status, code, message }
+    res.status(code).json(data)
+  }
 }
 
 export default postTripController
