f4faadfe0f4fb6530d707f681f4e3492ff4954e1
Keith Johnson <kjohnson0451@gmail.com>
2023-10-30 16:33:46 -0400

    Adjusted post trip controller

--- src/config/strings.js
+++ src/config/strings.js
@@ -4,3 +4,5 @@ export const MainConsoleMessage = `Server is running on port ${ServerPort}`
 export const Error404Message = "<h1>404 Endpoint not Found</h1>"
 export const RootMessage =
   "<h1>Welcome to the root endpoint. This app is running fine.</h1>"
+
+export const createTripSuccessMessage = "Successfully created trip."
--- src/controllers/trips/post_trip_controller.js
+++ src/controllers/trips/post_trip_controller.js
@@ -1,10 +1,16 @@
 import createTrip from "@services/trips/create_trip"
+import { createTripSuccessMessage } from "@config/strings"
 
 const postTripController = async (req, res) => {
   const tripData = req.body
   const trip = await createTrip(tripData)
-  const returnedTripData = trip.get({ plain: true })
-  res.status(201).json(returnedTripData)
+
+  const statusCode = 201
+  const message = createTripSuccessMessage
+  const { id } = trip
+
+  const returnedData = { statusCode, message, id }
+  res.status(201).json(returnedData)
 }
 
 export default postTripController
